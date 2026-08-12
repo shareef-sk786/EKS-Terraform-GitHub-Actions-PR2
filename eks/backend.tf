ac69a44642cc2a7deb01f6fe0b19451156a115f0
shareef-shaik <148448681+shareef-sk786@users.noreply.github.com>
2026-08-12 16:40:30 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,17 +1,11 @@
 terraform {
   required_version = "~> 1.15.8"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
-  }
-  backend "s3" {
-    bucket         = "dev-shareef-tf-bucket"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
   }
 }
 
